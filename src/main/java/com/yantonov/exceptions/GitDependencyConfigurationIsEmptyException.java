c4ff92be8536ad07da8476ba5c25f70ceefc3add
Yury Antonov <yantonov@yandex.ru>
2016-01-27 09:46:09 +0500

    error message typo

--- src/main/java/com/yantonov/exceptions/GitDependencyConfigurationIsEmptyException.java
+++ src/main/java/com/yantonov/exceptions/GitDependencyConfigurationIsEmptyException.java
@@ -6,7 +6,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 public class GitDependencyConfigurationIsEmptyException extends MojoExecutionException {
     public GitDependencyConfigurationIsEmptyException(GitDependency dependency) {
 
-        super(String.format("Git dependency has instalid configuration. GroupId, artifactId, repositoryLocation are required fields. [%s:%s repo=%s]",
+        super(String.format("Git dependency has invalid configuration. GroupId, artifactId, repositoryLocation are required fields. [%s:%s repo=%s]",
                 dependency.getGroupId() == null ? "" : dependency.getGroupId(),
                 dependency.getArtifactId() == null ? "" : dependency.getArtifactId(),
                 dependency.getRepositoryLocation() == null ? "" : dependency.getRepositoryLocation()));
